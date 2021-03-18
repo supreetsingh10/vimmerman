046a2bfa9b6acec2b80349e9cf308d82ce75b60a
Supreet <singh.supreet14@gmail.com>
2021-03-18 23:20:59 +0530

    Updated vimmer

--- vimmer.sh
+++ vimmer.sh
@@ -7,14 +7,18 @@ echo "Setting up vim"
 echo "The ID is $ID_LIKE"
 
 case $ID_LIKE in 
-    arch)
+
+    "arch")
+       echo "Installing vim"
        sudo pacman -Syu vim
        curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
        rm ~/.vimrc 
        curl -o ~/.vimrc 'https://raw.githubusercontent.com/supreetsingh10/vimmerman/master/vimrc' 
        ;;
-   ubuntu debian)
+
+   "ubuntu debian")
+       echo "Installing vim"
        sudo apt update && sudo apt upgrade && sudo apt install vim
        curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/
